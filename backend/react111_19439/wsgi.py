02533b5cb629c37ea812e167638737410d82c6f5
Crowdbotics <team@crowdbotics.com>
2020-08-07 15:04:31 +0000

    Initial commit.

---
+++ backend/react111_19439/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for react111_19439 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'react111_19439.settings')
+
+application = get_wsgi_application()
